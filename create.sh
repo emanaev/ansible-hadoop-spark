bf78502f0266fc2b208e43364a6b40cd362c9d4f
emanaev <emanaev@gmail.com>
2019-12-30 12:13:05 +0300

    Fix some bugs + refactor

--- create.sh
+++ create.sh
@@ -3,7 +3,7 @@
 INST=cx21
 hcloud server create --image=ubuntu-18.04 --name=main --type=$INST --ssh-key=emanaev@Eugenes-MacBook-Pro.local
 hcloud server add-label main hadoop=master
-for NAME in node-1 node-2 node-3
+for NAME in node-1 node-2 node-3 node-4
 do
   hcloud server create --image=ubuntu-18.04 --name=$NAME --type=$INST --ssh-key=emanaev@Eugenes-MacBook-Pro.local
   hcloud server add-label $NAME hadoop=node
